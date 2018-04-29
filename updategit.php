c0fc82c1ad237fcf91b5f33c0cc29e07497f6a41
Bernard Maltais <bernard.maltais@canada.ca>
2018-04-29 13:55:42 -0400

    Add output for git pull

--- updategit.php
+++ updategit.php
@@ -1,3 +1,11 @@
 <?php
-exec("git pull");
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+}
+// Print the exec output inside of a pre element
+print("<pre>" . execPrint("git pull") . "</pre>");
 ?>
