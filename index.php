7214ccad0504c9506b346aa5fc28936b8a508b5a
bernardmaltais <bernard.maltais@canada.ca>
2018-04-28 22:47:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
-Echo "<html>";
-Echo "<title>HTML With PHP</title>";
-
-echo "<style>
-body, html {
-    height: 100%;
-    margin: 0;
-}
-.bg {
-    /* The image used */
-    background-image: url("day.jpg");
-    /* Full height */
-    height: 100%; 
-    /* Center and scale the image nicely */
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-}
-</style>";
-
-Echo "<b>My Example</b>";
-echo "<br />";
+include('top.html');
 echo "<b>This page was served by container: </b>";
 print $_SERVER["SERVER_ADDR"];
 echo "<br />";
