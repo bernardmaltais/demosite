17edc8ea205d025a8c8165d3446ee16bbe38df40
bernardmaltais <bernard.maltais@canada.ca>
2018-04-28 22:38:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ echo "<br />";
 echo "<b>This page was served by container: </b>";
 print $_SERVER["REMOTE_ADDR"];
 echo "<br />";
-echo "<b>Passing through load balancer: </b>"
+echo "<b>Passing through load balancer: </b>";
 print $_SERVER["SERVER_ADDR"];
 echo "<br />";
-
+echo "<b>Exiting through node: </b>";
+print $_SERVER["HTTP_HOST"];
+echo "<br />";
 phpinfo();
 ?>
