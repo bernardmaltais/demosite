dd6437ff057588124187ae2dba1fbc82837e339a
bernardmaltais <bernard.maltais@canada.ca>
2018-04-28 23:21:43 -0400

    Update erasedatascript.php

--- erasedatascript.php
+++ erasedatascript.php
@@ -1,1 +1,12 @@
-
+<?php
+    $data = "";
+    $ret = file_put_contents('mydata.txt', $data, LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file";
+        header('Location: ' . $_SERVER["HTTP_REFERER"] );
+        exit;
+    }
+?>
