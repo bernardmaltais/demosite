80994b4ffdb6e2854badb12762b927221d53d0bd
bernardmaltais <bernard.maltais@canada.ca>
2018-04-28 22:28:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 Echo "<html>";
 Echo "<title>HTML With PHP</title>";
 Echo "<b>My Example</b>";
+Echo "<b></b>";
+echo "<b>This page was served from container: </b>";
 print $_SERVER["REMOTE_ADDR"];
+Echo "<b></b>";
 Print "<i>Print works too!</i>";
 ?>
